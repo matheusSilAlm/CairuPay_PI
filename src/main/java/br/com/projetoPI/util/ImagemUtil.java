a0202772e68006ca7bc6993889669b17ad49f06e
Matheus Silva <teushiftz@gmail.com>
2025-11-02 20:11:41 -0300

    Primeiro Commit -Tela login -Tela Dash(começo)

--- src/main/java/br/com/projetoPI/util/ImagemUtil.java
+++ src/main/java/br/com/projetoPI/util/ImagemUtil.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package utilitarios;
+package br.com.projetoPI.util;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import java.io.IOException;
